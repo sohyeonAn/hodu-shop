f657605465edf4d861fa3a404c570f9373731f11
sohyeonAn <ansh4393@gmail.com>
2022-01-25 18:01:30 +0900

    🎨 home페이지에서 product 불러오기

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,22 +1,20 @@
 import React from "react";
 import "../css/Product.css";
 
-const Product = () => {
+const Product = (props) => {
   return (
     <div className="product__container">
       <div className="product__imageContainer">
         <img
-          src="images/product1.png"
+          src={props.product.image}
           alt="상품이미지"
           className="product__image"
         />
       </div>
       <div className="product__detail">
-        <span className="product__company">우당탕탕 라이캣의 실험실</span>
-        <span className="product__name">
-          Hack Your Life 개발자 노트북 파우치
-        </span>
-        <span className="product__price">29,000원</span>
+        <span className="product__company">{props.product.seller_store}</span>
+        <span className="product__name">{props.product.product_name}</span>
+        <span className="product__price">{props.product.price}원</span>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
+import { API_ENDPOINT } from "../constants";
+import axios from "axios";
 import "../css/Home.css";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios({
+      url: `${API_ENDPOINT}/products/`,
+      method: "get",
+    })
+      .then((res) => {
+        setProducts(res.data.results);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }, []);
   return (
     <div className="home">
       <section className="home__sliderContainer">
@@ -10,11 +25,9 @@ function Home() {
       </section>
 
       <section className="home__productContainer">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {products.map((product) => {
+          return <Product key={product.product_id} product={product} />;
+        })}
       </section>
     </div>
   );
