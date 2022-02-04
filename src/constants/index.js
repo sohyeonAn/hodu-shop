df0309c64b595fedb7c2f181be11ed726b38c3ba
sohyeonAn <ansh4393@gmail.com>
2022-02-04 14:07:57 +0900

    🚚 서버 URL 바뀜

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,8 @@
-export const API_ENDPOINT = 'http://3.38.108.122:8000';
+// export const API_ENDPOINT = 'http://3.38.108.122:8000';
+export const API_ENDPOINT = "http://3.34.51.181:8000";
 
 export const MEDIA_QUERY_END_POINT = {
-  MOBILE: '640px',
-  TABLET: '1280px',
-  DESKTOP: '1920px',
+  MOBILE: "640px",
+  TABLET: "1280px",
+  DESKTOP: "1920px",
 };
